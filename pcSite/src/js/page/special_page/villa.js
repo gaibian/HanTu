3a4c574dd1d86ea44bd419dc3675106c75f33d18
caicaidawang <906809172@qq.com>
2018-01-02 22:34:18 +0800

    ym day1

--- pcSite/src/js/page/special_page/villa.js
+++ pcSite/src/js/page/special_page/villa.js
@@ -1,6 +1,18 @@
 require('../../../css/page/special_page/villa.less');
 $(()=>{
     "use strict";
+    const lazy = require('../../component/lazy');
+    let $lazy = document.getElementsByClassName('lazy');
+    lazy.one($lazy, function() {
+    	"use strict";
+    	let srcValue = this.getAttribute('data-src');
+    	this.setAttribute('src', srcValue);
+    	this.className = this.className + ' la';
+    });
+    
     const carousel = require('../../component/carousel');
     carousel('.carousel');
+    
+    const tabContent = require('../../component/tab_content');
+    tabContent('.tab_box');
 });
