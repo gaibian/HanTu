a1eb98d391b5a817bb6b5febed16c723b9676fe7
xiaojun092666 <xiaojun19890926@qq.com>
2018-01-03 19:55:43 +0800

    xj commit

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,6 +1,6 @@
 
 /*cdn服务器和本地虚拟服务器转换*/
-let serverFlag = true;  //true 打包启动cdn地址  false启动本地虚拟地址
+let serverFlag = false;  //true 打包启动cdn地址  false启动本地虚拟地址
 let site = 'pc';
 let csCdn = '';
 let siteStatic = site + 'Static';
@@ -8,7 +8,7 @@ let siteStatic = site + 'Static';
 serverFlag ? siteStatic = './' + siteStatic : siteStatic = '/'+siteStatic;
 module.exports = {
     site:site + 'Site',  //打包配置启动根目录
-    siteStatic:siteStatic,  //静态服务器地址文件根目录
+    siteStatic:site + 'Static',  //静态服务器地址文件根目录
     scCdn:'http://cdn.msphcn.com',  //正式服务器cdn地址
     csCdn:csCdn,  //测试服务器cdn地址
 };
