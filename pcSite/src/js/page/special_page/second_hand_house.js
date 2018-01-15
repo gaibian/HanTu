3c9f5d8fa0a1854b3e33e80df48454a084fc9479
xiaojun092666 <xiaojun19890926@qq.com>
2018-01-15 21:01:52 +0800

    xj commit

---
+++ pcSite/src/js/page/special_page/second_hand_house.js
@@ -0,0 +1,16 @@
+require('../../../css/page/special_page/second_hand_house.less');
+
+$(()=>{
+    "use strict";
+    const lazy = require('../../component/lazy');
+    let $lazy = document.getElementsByClassName('lazy');
+    lazy.one($lazy,function(){
+        "use strict";
+        let srcValue = this.getAttribute('data-src');
+        this.setAttribute('src',srcValue);
+        this.className = this.className + ' la';
+    });
+
+    const select = require('../../component/select');
+    select('.select_box');
+});
