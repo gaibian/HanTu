3d04be02e6ea82f0af3b5851effb063ff5b753ff
caicaidawang <906809172@qq.com>
2018-01-26 23:23:48 +0800

    ym complete

---
+++ pcSite/src/js/page/raiders_page/complete.js
@@ -0,0 +1,24 @@
+require('../../../css/page/raiders_page/complete.less');
+
+$(()=>{
+    "use strict";
+    const lazy = require('../../component/lazy');
+    let $lazy = document.getElementsByClassName('lazy');
+    lazy.one($lazy,function(){
+        "use strict";
+        let srcValue = this.getAttribute('data-src');
+        this.setAttribute('src',srcValue);
+        this.className = this.className + ' la';
+    });
+
+    const carousel = require('../../component/carousel');
+    carousel('.carousel');
+
+
+    let pageBtmFixed = $('.page_btm_fixed');
+    let btmCloseBtn = pageBtmFixed.find('.close_btn');
+    btmCloseBtn.on('click',function(){
+        pageBtmFixed.hide();
+    })
+
+});
