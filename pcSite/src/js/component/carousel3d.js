0feaf72462fc828da0e303c4e2b2cacc63a6cdc5
caicaidawang <906809172@qq.com>
2018-02-10 18:30:29 +0800

    ym

--- pcSite/src/js/component/carousel3d.js
+++ pcSite/src/js/component/carousel3d.js
@@ -2,60 +2,110 @@ const carousel3d = (function(){
 	"use strict";
 	const _queue = [];
 
-	function carousel3dFun(dom){
-		this.dom      = dom;
-		this.li       = this.dom.find('.flip_item');
-		this.classArr = ['flip_you1','flip_you2','flip_you3','flip_zuo3','flip_zuo2','flip_zuo1'];
-		this.text     = this.li.find('.flip_text_box');
-		this.current  = 0;
-		this.bind();
-		this.show();
-	};
-
-	// carousel3dFun.prototype.init = function(){
-
+	// function carousel3dFun(dom){
+	// 	this.dom      = dom;
+	// 	this.li       = this.dom.find('.flip_item');
+	// 	this.classArr = ['flip_you1','flip_you2','flip_you3','flip_zuo3','flip_zuo2','flip_zuo1'];
+	// 	this.text     = this.li.find('.flip_text_box');
+	// 	this.current  = 0;
+	// 	this.bind();
+	// 	this.show();
 	// };
 
-	carousel3dFun.prototype.bind = function(){
-		let that = this;
-		this.li.on('click',function(){
-			let i = that.li.index($(this));
-			let k = that.li.length - 1;
-			let chaNum = k - i;
-			let jianIndex = 0;
-			let addIndex = 0;
-			that.li.each(function(index){
-			    if(i === index){
-			        that.li.eq(index).attr('class','flip_item flip_current');
+	// carousel3dFun.prototype.bind = function(){
+	// 	let that = this;
+	// 	this.li.on('click',function(){
+	// 		let i = that.li.index($(this));
+	// 		let k = that.li.length - 1;
+	// 		let chaNum = k - i;
+	// 		let jianIndex = 0;
+	// 		let addIndex = 0;
+	// 		that.li.each(function(index){
+	// 		    if(i === index){
+	// 		        that.li.eq(index).attr('class','flip_item flip_current');
+	// 		    }
+	// 		    if(index < i){
+	// 		        that.li.eq(index).attr('class','flip_item '+ that.classArr[chaNum]);
+	// 		        chaNum ++;
+	// 		    }
+	// 		    if(index > i){
+	// 		        that.li.eq(index).attr('class','flip_item '+ that.classArr[addIndex]);
+	// 		        addIndex ++;
+	// 		    }
+	// 		})
+	// 	})
+	// };
+
+	// carousel3dFun.prototype.show = function(){
+	// 	let that = this;
+	// 	this.li.on('mouseover',function(){
+	// 	    let index = that.li.index($(this));
+	// 	    if($(this).hasClass('flip_current')){
+	// 	        $(this).find('.flip_text_box').show();
+	// 	    }
+	// 	});
+	// 	this.li.on('mouseleave',function(){
+	// 	    let index = that.li.index($(this));
+	// 	    if($(this).hasClass('flip_current')){
+	// 	        $(this).find('.flip_text_box').hide();
+	// 	    }
+	// 	})
+	// }
+
+	class carousel3dFun{
+		constructor(dom){
+			this.dom      = dom;
+			this.li       = this.dom.find('.flip_item');
+			this.classArr = ['flip_you1','flip_you2','flip_you3','flip_zuo3','flip_zuo2','flip_zuo1'];
+			this.text     = this.li.find('.flip_text_box');
+			this.current  = 0;
+			this.bind();
+			this.show();
+		}
+
+		bind(){
+			let that = this;
+			that.li.on('click',function(){
+				let i = that.li.index( $(this) );
+				let k = that.li.length - 1;
+				let chaNum = k - i;
+				let jianIndex = 0;
+				let addIndex = 0;
+				that.li.each((index)=>{
+				    if(i === index){
+				        that.li.eq(index).attr('class','flip_item flip_current');
+				    }
+				    if(index < i){
+				        that.li.eq(index).attr('class','flip_item '+ that.classArr[chaNum]);
+				        chaNum ++;
+				    }
+				    if(index > i){
+				        that.li.eq(index).attr('class','flip_item '+ that.classArr[addIndex]);
+				        addIndex ++;
+				    }
+				})
+			})
+
+		}
+
+		show(){
+			let that = this;
+			that.li.on('mouseover',function(){
+			    let index = that.li.index($(this));
+			    if($(this).hasClass('flip_current')){
+			        $(this).find('.flip_text_box').show();
 			    }
-			    if(index < i){
-			        that.li.eq(index).attr('class','flip_item '+ that.classArr[chaNum]);
-			        chaNum ++;
-			    }
-			    if(index > i){
-			        that.li.eq(index).attr('class','flip_item '+ that.classArr[addIndex]);
-			        addIndex ++;
+			});
+			that.li.on('mouseleave',function(){
+			    let index = that.li.index($(this));
+			    if($(this).hasClass('flip_current')){
+			        $(this).find('.flip_text_box').hide();
 			    }
 			})
-		})
-	};
+		}
 
-	carousel3dFun.prototype.show = function(){
-		let that = this;
-		this.li.on('mouseover',function(){
-		    let index = that.li.index($(this));
-		    if($(this).hasClass('flip_current')){
-		        $(this).find('.flip_text_box').show();
-		    }
-		});
-		this.li.on('mouseleave',function(){
-		    let index = that.li.index($(this));
-		    if($(this).hasClass('flip_current')){
-		        $(this).find('.flip_text_box').hide();
-		    }
-		})
+
 	}
-
 
 
 	return (el)=>{
