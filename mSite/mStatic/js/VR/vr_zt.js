aec85e72a332bc640876253f1a384cbba24ee9cf
xiaojun092666 <xiaojun19890926@qq.com>
2018-02-23 19:52:22 +0800

    xj commit

--- mSite/mStatic/js/VR/vr_zt.js
+++ mSite/mStatic/js/VR/vr_zt.js
@@ -10344,6 +10344,13 @@ __webpack_require__(64);
 
 $(function () {
     "use strict";
+
+    var mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0
+    });
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
